29a972a48ab447e8b971ddde9c028a3e79ca89a5
SAMAKSH10 <108063107+SAMAKSH10@users.noreply.github.com>
2023-06-03 03:17:53 -1200

    free
    free

--- Weather_Forecasting_Tool.py
+++ Weather_Forecasting_Tool.py
@@ -1,16 +1,20 @@
 import requests
+from datetime import date
 
-city = input("ENTER YOUR CITY")
+city = input("ENTER YOUR CITY : ")
 response = requests.get('https://goweather.herokuapp.com/weather/' + city)
-#print( response.status_code)
-#print(response.json())
-temperature = response.json()['temperature']
-print(temperature)
-wind = response.json()['wind']
-print(wind)
-description = response.json()['description']
-print(description)
+# print( response.status_code)
+# print(response.json())gwa
+# temperature = response.json()['temperature']
+# print(temperature)
+# wind = response.json()['wind']
+# print(wind)
+# description = response.json()['description']
+# print(description)
+s=['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+t=date.today()
 for x in range(0, 3):
-  print(response.json()['forecast'][x]['day'])
-  print(response.json()['forecast'][x]['temperature'])
-  print(response.json()['forecast'][x]['wind'])
+    print("Day :",response.json()['forecast'][x]['day'],end=" , ")
+    print (s[(t.weekday()+x)%7])
+    print("Temprature :",response.json()['forecast'][x]['temperature'])
+    print("Wind Speed :",response.json()['forecast'][x]['wind'],"\n")
